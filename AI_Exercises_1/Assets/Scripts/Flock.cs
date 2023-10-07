8850ffa920f0cda6d10ca6df5281675d28969ac0
JanNetoLlorens <99729455+JanNetoLlorens@users.noreply.github.com>
2023-10-07 19:46:15 +0200

    Flocking Done but could be better

--- AI_Exercises_1/Assets/Scripts/Flock.cs
+++ AI_Exercises_1/Assets/Scripts/Flock.cs
@@ -4,75 +4,147 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
-    public GameObject myManager;
+    float speed, minS, maxS;
+    Vector3 LiderPos = Vector3.zero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = Random.Range(FlockingManager.myManager.Min_Speed, FlockingManager.myManager.Max_Speed);
+        minS = FlockingManager.myManager.Min_Speed;
+        maxS = FlockingManager.myManager.Max_Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Cohesion();
-        MatchVelocity();
-        Separation();
+        Bounds hiveBox = new Bounds(FlockingManager.myManager.transform.position, FlockingManager.myManager.hiveLimits);
+        if (!hiveBox.Contains(transform.position))
+        {
+            Vector3 direction = FlockingManager.myManager.transform.position - transform.position;
+            transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                  Quaternion.LookRotation(direction),
+                                                  FlockingManager.myManager.Rotation_Speed * Time.deltaTime);
+        }
+
+        if (minS != FlockingManager.myManager.Min_Speed || maxS != FlockingManager.myManager.Max_Speed)
+        {
+            speed = Random.Range(FlockingManager.myManager.Min_Speed, FlockingManager.myManager.Max_Speed);
+        }
+
+        if (Random.Range(0, 100) < FlockingManager.myManager.freqFlock)
+        {
+            //ApplyRules();
+            AllTogether();
+        }
+        
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
     }
 
-    void Cohesion()
+    void ApplyRules()
     {
-        Vector3 cohesion = Vector3.zero;
-        int num = 0;
-        foreach (GameObject go in myManager.allBees)
+        GameObject[] hive;
+        hive = FlockingManager.myManager.allBees;
+
+        Vector3 vcenter = Vector3.zero;
+        Vector3 vavoid = Vector3.zero;
+        float gSpeed = 0.01f;
+        float nDistance;
+        int groupSize = 0;
+
+        foreach (GameObject go in hive)
         {
             if (go != this.gameObject)
             {
-                float distance = Vector3.Distance(go.transform.position, transform.position);
-                if (distance <= myManager.neighbourDistance)
+                nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                if (nDistance <= FlockingManager.myManager.Neighbour_Dist)
+                {
+                    vcenter += go.transform.position;
+                    groupSize++;
+
+                    if (nDistance < FlockingManager.myManager.Separation_Dist)
+                        vavoid += (this.transform.position - go.transform.position);
+
+                    Flock anotherFlock = go.GetComponent<Flock>();
+                    gSpeed = gSpeed + anotherFlock.speed;
+                }
+            }
+            if (groupSize > 0)
+            {
+                vcenter = vcenter / groupSize + (FlockingManager.myManager.goalPos - transform.position);
+                speed = gSpeed / groupSize;
+                if (speed > FlockingManager.myManager.Max_Speed)
+                    speed = FlockingManager.myManager.Max_Speed;
+
+                Vector3 direction = (vcenter + vavoid) - transform.position;
+                if (direction != Vector3.zero)
+                    transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                          Quaternion.LookRotation(direction),
+                                                          FlockingManager.myManager.Rotation_Speed * Time.deltaTime);
+            }
+        }
+    }
+
+    void AllTogether()
+    {
+        GameObject[] hive;
+        hive = FlockingManager.myManager.allBees;
+
+        Vector3 cohesion = Vector3.zero;
+        Vector3 separation = Vector3.zero;
+        float gSpeed = 0.01f;
+        float distance;
+        int num = 0;
+
+        foreach (GameObject go in hive)
+        {
+            if (go != this.gameObject)
+            {
+                distance = Vector3.Distance(go.transform.position, transform.position);
+                if (distance <= FlockingManager.myManager.Neighbour_Dist)
                 {
                     cohesion += go.transform.position;
+
+                    if (distance < FlockingManager.myManager.Separation_Dist)
+                    {
+                        separation += (transform.position - go.transform.position);
+                    }
+
+                    gSpeed += go.GetComponent<Flock>().speed;
                     num++;
                 }
             }
-        }
-        if (num > 0)
-            cohesion = (cohesion / num - transform.position).normalized * speed;
-    }
-
-    void MatchVelocity()
-    {
-        Vector3 align = Vector3.zero;
-        int num = 0;
-        foreach (GameObject go in myManager.allBees)
-        {
-            if (go != this.gameObject)
+            if (num > 0)
             {
-                float distance = Vector3.Distance(go.transform.position, transform.position);
-                if (distance <= myManager.neighbourDistance)
+                //cohesion = cohesion / num + (FlockingManager.myManager.goalPos - transform.position);
+                cohesion = ((cohesion / num - transform.position).normalized * speed);
+                if (FlockingManager.myManager.RandGoal)
+                    cohesion += (FlockingManager.myManager.goalPos - transform.position);
+
+                speed = gSpeed / num;
+                if (speed > FlockingManager.myManager.Max_Speed)
+                    speed = FlockingManager.myManager.Max_Speed;
+
+
+                if (this.CompareTag("Lider"))
                 {
-                    align += go.GetComponent<Flock>().direction;
-                    num++;
+                    LiderPos = transform.position;
+                    Debug.Log(LiderPos);
                 }
-            }
-        }
-        if (num > 0)
-        {
-            align /= num;
-            speed = Mathf.Clamp(align.magnitude, myManager.minSpeed, myManager.maxSpeed);
-        }
-    }
 
-    void Separation()
-    {
-        Vector3 separation = Vector3.zero;
-        foreach (GameObject go in myManager.allBees)
-        {
-            if (go != this.gameObject)
-            {
-                float distance = Vector3.Distance(go.transform.position,
-                                                  transform.position);
-                if (distance <= myManager.neighbourDistance)
-                    separation -= (transform.position - go.transform.position) / (distance * distance);
+                Vector3 direction;
+                if(FlockingManager.myManager.FollowLider && !this.CompareTag("Lider"))
+                {
+                    direction = LiderPos - transform.position;
+                }
+                else
+                {
+                    direction = (cohesion + separation) - transform.position;
+                }
+
+                if (direction != Vector3.zero)
+                    transform.rotation = Quaternion.Slerp(transform.rotation,
+                                                          Quaternion.LookRotation(direction),
+                                                          FlockingManager.myManager.Rotation_Speed * Time.deltaTime);
             }
         }
     }
--- AI_Exercises_1/Assets/Scripts/FlockingManager.cs
+++ AI_Exercises_1/Assets/Scripts/FlockingManager.cs
@@ -5,38 +5,73 @@ using UnityEngine;
 public class FlockingManager : MonoBehaviour
 {
     //float freq = 0f;
+    public static FlockingManager myManager;
     public GameObject beePrefab;
-    public int numBee;
+    public GameObject LiderPrefab;
+    public int numBee = 20;
     public GameObject[] allBees;
-    private Vector3 randPosBee;
+    public Vector3 hiveLimits = new Vector3(5, 5, 5);
+    public Vector3 goalPos = Vector3.zero;
+    [Range(1, 100)] public int freqFlock = 10;
+    public bool RandGoal = false;
+    Vector3 randPos;
 
-    [Range(0, 100)] public float posBee_x = 0f;
-    [Range(0, 100)] public float posBee_y = 0f;
-    [Range(0, 100)] public float posBee_z = 0f;
+    [Header("Lider Settings")]
 
-    [Range(0, 10)] public float Max_Speed = 5f;
-    [Range(0, 10)] public float Min_Speed = 5f;
-    [Range(0, 10)] public float Neighbour_Dist = 5f;
-    [Range(0, 10)] public float Rotation_Speed = 1f;
+    public bool FollowLider = false;
+    int numLiders = 0;
+
+    [Header("Bee Settings")]
+
+    [Range(1, 10)] public float Max_Speed;
+    [Range(1, 10)] public float Min_Speed;
+    [Range(1, 10)] public float Neighbour_Dist;
+    [Range(1, 10)] public float Rotation_Speed;
+    [Range(1, 10)] public float Separation_Dist = 1.0f;
 
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        allBees = new GameObject[numBee];
+        for (int i = 0; i < numBee; ++i)
+        {
+            randPos = this.transform.position + new Vector3(Random.Range(-hiveLimits.x, hiveLimits.x),
+                                                                Random.Range(-hiveLimits.y, hiveLimits.y),
+                                                                Random.Range(-hiveLimits.z, hiveLimits.z));
+            //Vector3 randomize = randPosBee;
+            allBees[i] = (GameObject)Instantiate(beePrefab, randPos, Quaternion.identity);
+        }
+        myManager = this;
+        goalPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        allBees = new GameObject[numBee];
-        for (int i = 0; i < numBee; ++i)
+        if(FollowLider && numLiders == 0)
         {
-            Vector3 pos = this.transform.position + randPosBee;
-            Vector3 randomize = randPosBee;
-            allBees[i] = (GameObject)Instantiate(beePrefab, pos, Quaternion.LookRotation(randomize));
-            //allBees[i].GetComponent<Flock>().myManager = this;
+            LiderPrefab = Instantiate(LiderPrefab, randPos, Quaternion.identity);
+            LiderPrefab.tag = "Lider";
+            numLiders++;
+        }
+        
+        if(!FollowLider)
+        {
+            LiderPrefab.SetActive(false);
+        }
+        else
+        {
+            LiderPrefab.SetActive(true);
+        }
+      
+        
+        if (Random.Range(0, 100) < 1 && RandGoal)
+        {
+            goalPos = this.transform.position + new Vector3(Random.Range(-hiveLimits.x, hiveLimits.x),
+                                                            Random.Range(-hiveLimits.y, hiveLimits.y),
+                                                            Random.Range(-hiveLimits.z, hiveLimits.z));
         }
     }
 }
