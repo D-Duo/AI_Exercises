c0f284d4c876a5d35c1474a1f5ee8ffa968096c9
JanNetoLlorens <99729455+JanNetoLlorens@users.noreply.github.com>
2023-10-24 12:32:21 +0200

    Almost done just hiding needed

--- AI_Exercises_1/Assets/Scripts/FS_Machine.cs
+++ AI_Exercises_1/Assets/Scripts/FS_Machine.cs
@@ -7,51 +7,120 @@ public class FS_Machine : MonoBehaviour
 {
 
     public NavMeshAgent agent;
+    public NavMeshAgent police;
+    public GameObject key;
 
-    //wander
+    private float _stealDist = 1;
 
-    [Range(0, 100)] public float speed = 5f;
+    [Header("Wander Settings")]
+
+    [Range(0, 100)] public float WanderSpeed = 1f;
     [Range(1, 500)] public float walkRadius = 10f;
     [Range(0, 100)] public int maxRestingTime = 5;
     int restingTime = 1;
 
-    IEnumerator<int> fibonacci()
+    [Header("Approach Settings")]
+
+    //[Range(0, 100)] public float f
+    [Range(0, 100)] public float SafeDistance;
+    [Range(0, 10)] public float ApproachSpeed = 5f;
+
+    [Header("Hide Settings")]
+
+
+
+    [Header("Coroutine Settings")]
+
+    //public bool enabled = true;
+    private WaitForSeconds wait = new WaitForSeconds(0.05f);
+    delegate IEnumerator State();
+    private State state;
+
+    IEnumerator Start()
     {
-        int a = 0;              // Output:
-        int b = 1;              // 0,1,1,2,3,5,8,13,21,34...
-        yield return a;
+        state = Wander;
+
+        while (enabled)
+        {
+            yield return StartCoroutine(state());
+        }
+    }
+
+    IEnumerator Wander()
+    {
+        Debug.Log("Wander state");
+        agent.speed = WanderSpeed;
+
+        while (Vector3.Distance(police.transform.position, key.transform.position) < SafeDistance)
+        {
+            WanderF();
+
+            yield return wait;
+        };
+
+        state = Approach;
+    }
+
+    IEnumerator Approach()
+    {
+        Debug.Log("Approach state");
+
+        agent.speed = ApproachSpeed;
+        Seek(key.transform.position);
+
+        bool stolen = false;
+        while (Vector3.Distance(police.transform.position, key.transform.position) > SafeDistance)
+        {
+            if (Vector3.Distance(key.transform.position, transform.position) < ApproachSpeed)
+            {
+                stolen = true;
+                break;
+            };
+            yield return wait;
+        };
+
+        if (stolen)
+        {
+            Steal();
+            state = Hide;
+        }
+        else
+        {
+            agent.speed = 1f;
+            state = Wander;
+        }
+    }
+
+    IEnumerator Hide()
+    {
         while (true)
         {
-            yield return b;
-            int c = b;
-            b = a + b;
-            a = c;
+
+
+            yield return wait;
         }
     }
 
     // Start is called before the first frame update
-    void Start()
-    {
-        IEnumerator<int> f = fibonacci();
-        for (int i = 0; i < 10; i++)
-        {
-            f.MoveNext();
-            Debug.Log(f.Current);
-        }
-    }
+    //void Start()
+    //{
+
+    //}
 
     // Update is called once per frame
-    void Update()
-    {
-        Wander();
-    }
 
     void Seek(Vector3 destination)
     {
         agent.SetDestination(destination);
     }
 
-    void Wander()
+    void Steal()
+    {
+        key.GetComponent<Renderer>().enabled = false;
+        Debug.Log("Stolen");
+    }
+
+    void WanderF()
     {
         if (agent.remainingDistance <= agent.stoppingDistance)
         {
